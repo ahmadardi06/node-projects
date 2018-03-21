f8602c2b5c95b66d2ae648bf9cf981c6ca82f680
Ardiansyah <ardiansyah3ber@gmail.com>
2018-03-21 06:52:08 +0000

    update tampilan google maps, kontrolled

--- node-trackcar/routes/kontrolled.js
+++ node-trackcar/routes/kontrolled.js
@@ -4,6 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('kontrolled', { title: 'Kontrol LED' });
+
+  res.io.on('connection', (socket) => {
+    socket.on('statuslampu', (data) => {
+      console.log("status lampu: ", data.msg);
+    });
+  });
 });
 
 module.exports = router;

be0f5e295c2c7c7118ae08123e869e1cca160a46
Ardiansyah <ardiansyah3ber@gmail.com>
2018-03-22 00:46:57 +0000

    update and add login session with express-session

--- node-trackcar/routes/kontrolled.js
+++ node-trackcar/routes/kontrolled.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+//jalankan terminal
+var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +10,18 @@ router.get('/', function(req, res, next) {
   res.io.on('connection', (socket) => {
     socket.on('statuslampu', (data) => {
       console.log("status lampu: ", data.msg);
+    });
+
+    socket.on('statusgps', (data) => {
+      console.log("status gps: ", data.msg);
+      if(data.msg == 1)
+        exec('sudo systemctl start gps-python.service', (err, stout, sterr) => {
+          console.log("stout: ", stout);
+        });
+      else
+        exec('sudo systemctl stop gps-python.service', (err, stout, sterr) => {
+          console.log("stout: ", stout);
+        });
     });
   });
 });
